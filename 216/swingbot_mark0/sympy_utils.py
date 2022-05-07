48e1acebbc060914da93e213dcf8b71f3398cd65
5207 <cyanatg@gmail.com>
2022-05-07 12:50:12 -0700

    sympy_utils

---
+++ 216/swingbot_mark0/sympy_utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import sympy
+from sympy import *
+from sympy.physics.mechanics import *
+sympy.init_printing()
+
+def simplify_eq_with_assumptions(eq):
+    assert eq.rhs == 0  # assert that right-hand side is zero
+    print(type(eq.lhs))
+    assert type(eq.lhs) == Mul  # assert that left-hand side is a multipl.
+    newargs = []  # define a list of new multiplication factors.
+    for arg in eq.lhs.args:
+        # print(arg)
+        # print(type(arg))
+        # print(arg.is_positive)
+        if type(arg) == Symbol:
+            if arg.is_positive:
+                print("removing a constant", arg)
+                continue  # arg is positive, let's skip it.
+        newargs.append(arg)
+    # rebuild the equality with the new arguments:
+    return Eq(eq.lhs.func(*newargs), 0)
+
+def pull_out_term(expr, term, product_candidates = [], default=0):
+    ret = 0
+    found = False
+
+    product_candidates.append(1)
+    for c in product_candidates:
+        d = collect(expr, term*c, evaluate=False)
+        if term*c in d:
+            found = True
+            print("found")
+            ret += c*d[term*c]
+
+    leftover = simplify(expr - ret * term)
+
+    if not found:
+        print("not found")
+        ret = default
+
+    return ret, leftover
+
+def sympy_to_expression(sympy_expr):
+    expr = python(sympy_expr).split("\n")[-1].split(" = ")[-1]
+
+    expr = expr.replace("p1_damping", "Q1_DAMPING")
+
+    expr = expr.replace("t2(t)", "t2")
+    expr = expr.replace("t1(t)", "t1")
+    expr = expr.replace("Derivative(t2, t)", "t2_dot")
+    expr = expr.replace("Derivative(t1, t)", "t1_dot")
+    expr = expr.replace("g", "G")
+    expr = expr.replace("m", "M")
+    expr = expr.replace("l", "L")
+    return expr
