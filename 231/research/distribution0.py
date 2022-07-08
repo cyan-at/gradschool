c8848d6210259c88fb3bd08c59eb3f4ab877c8f4
cyan <cyanatg@gmail.com>
2022-07-08 11:03:05 -0700

    #cool using numpy broadcasting to speed up computation 5s to 0.1s, and gist_heat colormap will show probability without washing out other colors

--- 231/research/distribution0.py
+++ 231/research/distribution0.py
@@ -1,30 +1,13 @@
 #!/usr/bin/python3
 
+import argparse
+
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
 import numpy as np
-import os, pickle
-
-alpha2 = 1.0
-
-# time sample t = 0
-t = 0.0
-
-# p0 is distribution with mean at 0.5
-window = 5.0
-N = 50
-mu_0 = np.array([5.5]*3)
-cov_0 = np.eye(3)
-
-colors = [
-    (0, 0, 0),
-    (45, 5, 61),
-    (84, 42, 55),
-    (150, 87, 60),
-    (208, 171, 141),
-    (255, 255, 255)
-]
+import os, pickle, time, sys
+from matplotlib import cm
 
 def normal_dist_array(x , mean , cov_matrix):
     '''
@@ -43,9 +26,12 @@ def inverse_flow(x1, x2, x3, t, alpha2):
     '''
     inverse flow map [x1, x2, x3] -> [x10, x20, x30]
     '''
-    # x1 = state[0]
-    # x2 = state[1]
-    # x3 = state[2]
+    if (np.abs(x1) < 1e-8 and np.abs(x2) < 1e-8):
+
+        print("noo")
+        raise Exception("singularity")
+    elif (t < 1e-8):
+        return np.array([x1, x2, x3])
 
     alpha2 = 1.0
 
@@ -57,6 +43,14 @@ def inverse_flow(x1, x2, x3, t, alpha2):
     x30 = x3
 
     return np.array([x10, x20, x30])
+
+def composited(x1, x2, x3, t, alpha2, mean, covariance):
+    try:
+        inversed_state = inverse_flow(x1, x2, x3, t, alpha2)
+        return normal_dist_array(inversed_state, mean, covariance)
+    except Exception as e:
+        print(str(e))
+        return 0.0
 
 def Sphere(rows, cols, func, args=None):
     verts = np.empty((rows+1, cols, 3), dtype=float)
@@ -93,13 +87,67 @@ def Sphere(rows, cols, func, args=None):
 
     return gl.MeshData(vertexes=verts, faces=faces)
 
+colors = [
+    (0, 0, 0),
+    (45, 5, 61),
+    (84, 42, 55),
+    (150, 87, 60),
+    (208, 171, 141),
+    (255, 255, 255)
+]
+cmap = pg.ColorMap(pos=np.linspace(0.0, 0.5, len(colors)), color=colors)
+cmap = cm.get_cmap('gist_heat')
+
+#############################################################################
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--times',
+    type=str,
+    default="0,1,5",
+    required=False)
+
+parser.add_argument('--mu_0',
+    type=float,
+    default=4.5,
+    required=False)
+
+parser.add_argument('--sampling',
+    type=str,
+    default="15,5,15,5,5,5,100",
+    required=False)
+
+args = parser.parse_args()
+
+# system
+alpha2 = 1.0
+
+# distribution
+mu_0 = np.array([args.mu_0]*3)
+cov_0 = np.eye(3)
+
+# sampling
+ts = [float(x) for x in args.times.split(",")]
+sampling = [int(x) for x in args.sampling.split(",")]
+
+window0 = sampling[0]
+window1 = sampling[1]
+
+window2 = sampling[2]
+window3 = sampling[3]
+
+window4 = sampling[4]
+window5 = sampling[5]
+
+N = sampling[6]
+
 #############################################################################
 
 ## Create a GL View widget to display data
 app = QtGui.QApplication([])
 w = gl.GLViewWidget()
 w.show()
-w.setWindowTitle('pyqtgraph example: GLSurfacePlot')
+w.setWindowTitle('snapshots')
 w.setCameraPosition(distance=20)
 
 #############################################################################
@@ -112,129 +160,143 @@ w.addItem(g)
 
 #############################################################################
 
-x1 = np.linspace(mu_0[0] - window, mu_0[0] + window, N)
-x2 = np.linspace(mu_0[1] - window, mu_0[1] + window, N)
-x3 = np.linspace(mu_0[2] - window, mu_0[2] + window, N)
+x1 = np.linspace(mu_0[0] - window0, mu_0[0] + window1, N)
+x2 = np.linspace(mu_0[1] - window2, mu_0[1] + window3, N)
+x3 = np.linspace(mu_0[2] - window4, mu_0[2] + window5, N)
 
-fname = 'inversed_%d_%d_%d.pkl' % (N, int(window), int(t))
-inversed0 = None
-inversed1 = None
-inversed2 = None
-if os.path.exists(fname):
-    print("%s found" % (fname))
-    with open(fname, 'rb') as handle:
-        data = pickle.load(handle)
-        inversed0 = data["inversed0"]
-        inversed1 = data["inversed1"]
-        inversed2 = data["inversed2"]
-else:
-    '''
-    X1, X2, X3 = np.meshgrid(x1,x2,x3) # each is NxNxN
+with_args = lambda x, y, z: composited(x, y, z, 0, alpha2, mu_0, cov_0)
 
-    pos = np.empty(X1.shape + (4,)) # NxNxN, for x10, x20, x30, p0
+start = time.time()
 
-    t = 0
-    omega = alpha2 * X3
-    gamma = (X2 - X1 * np.tan(omega*t)) / (X1 + X2*np.tan(omega*t))
+#############################################################################
+# for loop: 5s
 
-    pos[:, :, :, 0] = np.sqrt((X1**2 + X2**2) / (1 + gamma))
-    pos[:, :, :, 1] = gamma * np.sqrt((X1**2 + X2**2) / (1 + gamma**2))
-    pos[:, :, :, 2] = X3
+# temp = np.zeros((N, N, N, 1))
+# for i in range(N):
+#     for j in range(N):
+#         for k in range(N):
+#             # pos = inverse_flow(x1[i], x2[j], x3[k], 0, alpha2)
+#             # temp[i, j, k, 3] = normal_dist_array(pos, mu_0 , cov_0)
+#             temp[i, j, k, 0] = with_args(x1[i], x2[j], x3[k])
 
-    pos[:, :, :, 3] = 
+#############################################################################
+# bad vectorize: 5s
 
-    # this takes the linspace at that axis
-    # and duplicates it across the other axii
-    # so instead of iterating, we sample
-    # taking more space to take less time
+# with_args_vec = np.vectorize(with_args)
+# temp = with_args_vec(X1, X2, X3)
 
-    inversed = inverse_flow(X1, X2, X3) # 500x500x500, 3D matrix
+#############################################################################
+# using broadcasting: 0.12s
 
-    inversed1 = inverse_flow_t1(X1, X2, X3) # 500x500x500, 3D matrix
+X1, X2, X3 = np.meshgrid(x1,x2,x3,copy=False) # each is NxNxN
 
-    '''
+t_e = 0
+omegas = alpha2 * X3
+tans = np.tan(omegas*t_e)
+gammas = (X2 - X1 * tans) / (X1 + X2*tans)
 
-    inversed0 = np.zeros((N, N, N, 4))
-    inversed1 = np.zeros((N, N, N, 4))
-    inversed2 = np.zeros((N, N, N, 4))
+x10 = np.sqrt((X1**2 + X2**2) / (1 + gammas))
+x20 = gammas * np.sqrt((X1**2 + X2**2) / (1 + gammas**2))
+x30 = X3
 
-    for i in range(N):
-        for j in range(N):
-            for k in range(N):
-                inversed0[i, j, k, :3] = inverse_flow(x1[i], x2[j], x3[k], 0, alpha2)
-                inversed0[i, j, k, 3] = normal_dist_array(inversed0[i, j, k, :3], mu_0 , cov_0)
+###################
 
-                inversed1[i, j, k, :3] = inverse_flow(x1[i], x2[j], x3[k], 1, alpha2)
-                inversed1[i, j, k, 3] = normal_dist_array(inversed1[i, j, k, :3], mu_0 , cov_0)
+den = (np.sqrt(2*np.pi*np.linalg.norm(cov_0)))
+cov_inv = np.linalg.inv(cov_0)
 
-                inversed2[i, j, k, :3] = inverse_flow(x1[i], x2[j], x3[k], 5, alpha2)
-                inversed2[i, j, k, 3] = normal_dist_array(inversed2[i, j, k, :3], mu_0 , cov_0)
-    with open(fname, 'wb') as handle:
-        pickle.dump(
-            {
-                "inversed0" : inversed0,
-                "inversed1" : inversed1,
-                "inversed2" : inversed2,
-            },
-            handle, protocol=pickle.HIGHEST_PROTOCOL)
+x10_diff = x10 - mu_0[0]
+x20_diff = x20 - mu_0[1]
+x30_diff = x30 - mu_0[2]
+
+# 3 x NxNxN to N**3 x 3
+x10_x20_x30 = np.vstack([x10_diff.reshape(-1), x20_diff.reshape(-1), x30_diff.reshape(-1)])
+
+probs = np.exp(np.einsum('i...,ij,j...',x10_x20_x30,cov_inv,x10_x20_x30)/(-2)) / den
+
+# probs = probs.reshape(N, N, N)
 
 #############################################################################
 
-size = np.ones((N**3)) * 0.2
+sample = np.random.multivariate_normal(mu_0, cov_0, 100) # 100 x 3
+print(sample.shape)
 
-pos0 = np.empty((N**3, 3))
-color0 = np.zeros((N**3, 4))
+size = np.ones(100) * 0.2
+g = (0, 255, 0, 255)
+gray = (0.5, 0.5, 0.5, 0.1)
+b = (0, 0, 255, 255)
 
-pos1 = np.empty((N**3, 3))
-color1 = np.zeros((N**3, 4))
+# import ipdb; ipdb.set_trace();
 
-pos2 = np.empty((N**3, 3))
-color2 = np.zeros((N**3, 4))
+if t_e > 0:
+    A = np.sqrt(sample[:, 0]**2 + sample[:, 1]**2)
+    phi = np.arctan2(sample[:, 1], sample[:, 0])
 
+    n_time_samples = 100
+    t_samples = np.linspace(0, t_e, n_time_samples)
 
-max_0 = np.max(inversed0[:, :, :, 3])
-max_1 = np.max(inversed1[:, :, :, 3])
-max_2 = np.max(inversed2[:, :, :, 3])
+    all_time_data = np.empty((sample.shape[0], sample.shape[1], n_time_samples))
+    # x/y slice is all samples at that time, 1 x/y slice per z time sample
 
-print("max_0", max_0)
-print("max_1", max_1)
-print("max_2", max_2)
+    # import ipdb; ipdb.set_trace();
 
-m = max([max_0, max_1, max_2])
+    for i, t_i in enumerate(t_samples):
+        x_1 = A * np.cos(alpha2 * sample[:, 2] * t_samples[i] + phi)
+        x_2 = A * np.sin(alpha2 * sample[:, 2] * t_samples[i] + phi)
+        x_3 = sample[:, 2]
+        # all are nx1 for t_i
 
-cmap = pg.ColorMap(pos=np.linspace(0.0, max_0, len(colors)), color=colors)
+        # stack it up
+        all_time_data[:, :, i] = np.vstack([x_1, x_2, x_3]).T
 
-for i in range(N):
-    for j in range(N):
-        for k in range(N):
-            pos0[i*N*N+j*N+k] = inversed0[i, j, k, :3]
-            # color0[i*N*N+j*N+k] = (inversed0[i, j, k, 3] * 5, 0.0, 0.0, 0.5)
+    # generate the line from looking at x / z slices
 
-            t = cmap.mapToQColor(inversed0[i, j, k, 3])
-            color0[i*N*N+j*N+k] = t.getRgbF()
+    for i in range(sample.shape[0]):
+        item = gl.GLLinePlotItem(
+            pos = all_time_data[i, :, :].T,
+            width = 0.5,
+            color = gray)
 
-            pos1[i*N*N+j*N+k] = inversed1[i, j, k, :3]            
-            # color1[i*N*N+j*N+k] = (0.0, inversed1[i, j, k, 3] * 5 , 0.0, 0.5)
+        w.addItem(item)
 
-            t = cmap.mapToQColor(inversed1[i, j, k, 3])
-            color1[i*N*N+j*N+k] = t.getRgbF()
+    ends = all_time_data[:, :, -1] # the top most time slice is the end
+    endpoints = gl.GLScatterPlotItem(pos=ends, size=size, color=b, pxMode=False)
+    w.addItem(endpoints)
 
-            pos2[i*N*N+j*N+k] = inversed2[i, j, k, :3]            
-            # color2[i*N*N+j*N+k] = (0.0, 0.0, inversed2[i, j, k, 3] * 5, 0.5)
+sp0 = gl.GLScatterPlotItem(pos=sample, size=size, color=g, pxMode=False)
 
-            t = cmap.mapToQColor(inversed2[i, j, k, 3])
-            color2[i*N*N+j*N+k] = t.getRgbF()
+#############################################################################
 
-sp0 = gl.GLScatterPlotItem(pos=pos0, size=size, color=color0, pxMode=False)
+end = time.time()
+
+total_time = end - start
+print("compute %s\n" % str(total_time))
+
+#############################################################################
+
+start = time.time()
+
+size = np.ones((N**3)) * 0.1
+pos = np.vstack([X1.reshape(-1), X2.reshape(-1), X3.reshape(-1)]).T
+color0 = cmap(probs)
+
+# # color0[:, 3] = color0[:, 3] / 10
+
+# color0[:, 3] /= 10
+print(color0[:, 3])
+
+sp1 = gl.GLScatterPlotItem(pos=pos, size=size, color=color0, pxMode=False)
+
+end = time.time()
+
+total_time = end - start
+print("render %s\n" % str(total_time))
+
+#############################################################################
+
+w.addItem(sp1)
 w.addItem(sp0)
 
-sp1 = gl.GLScatterPlotItem(pos=pos1, size=size, color=color1, pxMode=False)
-sp1.translate(5,5,0)
-w.addItem(sp1)
-
-sp2 = gl.GLScatterPlotItem(pos=pos2, size=size, color=color2, pxMode=False)
-sp2.translate(10,10,0)
-w.addItem(sp2)
+#############################################################################
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
