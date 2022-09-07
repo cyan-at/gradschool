9b0dd939dbca7c45b2fffbfa6adfc0e8a4afd6cd
cyan3 <cyan3@ndc.nasa.gov>
2022-09-06 17:16:10 -0700

    working

--- 231/research/linprognd_test.py
+++ 231/research/linprognd_test.py
@@ -7,8 +7,15 @@ from scipy.stats import multivariate_normal
 
 N = nSample = 100
 
-x_grid = np.transpose(np.linspace(0., 6., nSample))
-y_grid = np.transpose(np.linspace(0., 6., nSample))
+state_min = 0
+state_max = 6
+T_t = 5
+
+sigma_0 = 0.1
+sigma_T = 0.08
+
+x_grid = np.transpose(np.linspace(state_min, state_max, nSample))
+y_grid = np.transpose(np.linspace(state_min, state_max, nSample))
 [X,Y] = np.meshgrid(x_grid,x_grid)
 C = (X - Y)**2
 
@@ -29,31 +36,33 @@ A = np.concatenate(
 # 2*nSample
 
 mu_0 = 5.0
-mu_T = 1.0
+mu_T = 5.0
 rv0 = multivariate_normal([mu_0, mu_0, mu_0], sigma_0 * np.eye(3))
 rvT = multivariate_normal([mu_T, mu_T, mu_T], sigma_T * np.eye(3))
 
-def pdf1d_T(x):
-    mu = 5.
-    sigma = .1
-    a, b = (0. - mu) / sigma, (6. - mu) / sigma
-    rho_T=truncnorm.pdf(x, a, b, loc = mu, scale = sigma)
-    return rho_T
+def pdf1d_0(x,y,z):
+    return rv0.pdf(np.hstack((x, y, z)))
 
-def pdf1d_0(x):
-    sigma = .08
-    mu=0.5 
-    # the smaller this is, the smaller sigma must be to solve
-    # i.e.
-    # mu=0.6, sigma=0.1
-    # mu=0.5, sigma=0.08
-    a, b = (0. - mu) / sigma, (6. - mu) / sigma
-    rho_0=truncnorm.pdf(x, a, b, loc = mu, scale = sigma)
-    return rho_0
+def pdf1d_T(x,y,z):
+    return rvT.pdf(np.hstack((x, y, z)))
 
-x_T = np.transpose(np.linspace(0., 6., N))
-rho_0=pdf1d_0(x_T).reshape(len(x_T),1)
-rho_T=pdf1d_T(x_T).reshape(len(x_T),1)
+################################
+
+# 6 define data and net
+
+x_T = np.transpose(np.linspace(state_min, state_max, N))
+y_T = np.transpose(np.linspace(state_min, state_max, N))
+z_T = np.transpose(np.linspace(state_min, state_max, N))
+
+x_T=x_T.reshape(len(x_T),1)
+y_T=y_T.reshape(len(y_T),1)
+z_T=z_T.reshape(len(z_T),1)
+
+terminal_time=np.hstack((x_T,y_T,z_T,T_t*np.ones((len(x_T), 1))))
+
+rho_T=pdf1d_T(x_T,y_T,z_T).reshape(len(x_T),1)
+
+rho_0=pdf1d_0(x_T,y_T,z_T).reshape(len(x_T),1)
 
 bvector = np.concatenate((rho_0, rho_T), axis=0)
 res = linprog(cvector, A_eq=A, b_eq=bvector, options={"disp": True})
