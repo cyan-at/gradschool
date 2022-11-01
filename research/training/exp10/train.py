a960ec6a06f257c2fa425425d4be759e8791501a
cyan3 <cyan3@ndc.nasa.gov>
2022-10-31 17:54:26 -0700

    outputs only depends on # of inputs pde uses, not # pde outputs

--- research/training/exp10/train.py
+++ research/training/exp10/train.py
@@ -199,7 +199,7 @@ def get_model(
     # d+1 inputs: <state> + t
     # 5 outputs: 2 eq
     net = dde.nn.FNN(
-        [d+1] + [70] *3  + [5],
+        [d+1] + [70] *3  + [2],
         activations,
         init
         # "zeros",
