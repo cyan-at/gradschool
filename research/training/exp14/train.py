aba2aabf7d37b8d3b0812e374ee51192be362cc7
cyan <cyanatg@gmail.com>
2022-12-30 10:41:51 -0800

    fix

--- research/training/exp14/train.py
+++ research/training/exp14/train.py
@@ -271,8 +271,8 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, default=-1, help='')
     parser.add_argument('--restore', type=str, default="", help='')
     parser.add_argument('--batchsize',
-        type=int,
-        default=500)
+        type=str,
+        default="500")
 
     parser.add_argument('--diff_on_cpu',
         type=int, default=1)
