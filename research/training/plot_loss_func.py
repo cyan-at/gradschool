61e84c9d98193e16fc22288ef4328a053227fd3c
cyan <cyanatg@gmail.com>
2023-03-12 17:35:08 -0700

    sexiness

--- research/training/plot_loss_func.py
+++ research/training/plot_loss_func.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
 import time, os, sys
+plt.rcParams['text.usetex'] = True
 
 class Counter(object):
   def __init__(self):
@@ -53,18 +54,40 @@ if __name__ == '__main__':
 
   num_cols = int((num_cols - 1) / 2) + 1
 
+  label_lookup = {
+    1 : 'Controlled Fokker-Planck PDE',
+    2 : 'HJB PDE',
+  }
+
   for i in range(1, num_cols):
     data = loss_loaded[:, i]
     data = np.where(data < 1e-10, 10, data)
+
+    lbl = 'eq %d' % (i)
+    if i == num_cols - 2:
+      lbl = r'$\rho_0$ boundary condition'
+      data /= 5.0
+      data -= (10*0.01)
+    elif i == num_cols - 1:
+      lbl = r'$\rho_T$ boundary condition'
+      data /= 5.0
+      data -= (10*0.01)
+    elif i in label_lookup:
+      lbl = label_lookup[i]
+
     ax.plot(epoch, data,
       color=colors[i % len(colors)],
       lw=1,
-      label='eq %d' % (i))
+      label=lbl)
 
   ax.grid()
-  ax.legend(loc="lower left")
-  ax.set_title('training error/residual plots, %d epochs' % (len(epoch)))
+  ax.legend(loc="upper right", frameon=False)
+  # ax.set_title('training error/residual plots, %d epochs' % (len(epoch)))
+
   ax.set_yscale('log')
+  ax.set_ylabel('PINN residuals')
+
+  ax.set_xlabel('Epoch')
   ax.set_xscale('log')
 
   ax.yaxis.set_label_position("right")
